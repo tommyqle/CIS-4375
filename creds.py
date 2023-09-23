813859d1c3690e0af6c31c72df5f4bfadc00936a
hksiddi4 <hussainks92@gmail.com>
2023-09-23 14:19:57 -0500

    Framework, not final

---
+++ creds.py
@@ -0,0 +1,5 @@
+class Creds:
+    conString = 'cis4375falldb.cfav0re1cgdb.us-east-1.rds.amazonaws.com'
+    userName = 'admin'
+    password = 'adminadmin'
+    dbName = 'cis4375db'
---
+++ test.py
@@ -0,0 +1,39 @@
+import flask
+import json
+from flask import request, make_response
+from sql import create_connection, execute_query, execute_read_query
+import creds
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# Create connection to MySQL database
+myCreds = creds.Creds()
+conn = create_connection(myCreds.conString, myCreds.userName, myCreds.password, myCreds.dbName)
+
+# Test input data to database
+testData = [
+    {"name": "Hussain"}
+]
+
+@app.route('/', methods=['POST'])
+def testAdd():
+    test_dict = json.load(testData)
+    newName = test_dict['name']
+
+    post_statement = "INSERT INTO test (name) VALUES ('%s')" % (newName)
+    execute_query(conn, post_statement)
+    return "Successfully added!"
+
+
+
+
+
+
+
+
+
+
+
+
+app.run()
